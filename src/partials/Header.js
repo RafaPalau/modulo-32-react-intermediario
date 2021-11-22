59e2a3822c1d9072b9221ac6a7c71c5a5bb48c06
Rafael <rafaeldonizetip@gmail.com>
2021-11-22 15:13:00 -0300

    Aula 04 Criando o Header

---
+++ src/partials/Header.js
@@ -0,0 +1,25 @@
+import {
+  AppBar,
+  Button,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+
+const Header = () => {
+  return (
+    <AppBar position='static'>
+      <Toolbar>
+        <IconButton size='large' edge='start' color='inherit' aria-label='menu'>
+          <MenuIcon />
+        </IconButton>
+        <Typography variant='h6'>
+          News
+        </Typography>
+        <Button color='inherit'>Login</Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
+export default Header;
