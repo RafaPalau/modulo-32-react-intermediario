17afcd265237f3bac5de95da339cebfc7ae2b954
Rafael <rafaeldonizetip@gmail.com>
2021-11-25 10:08:06 -0300

    Aula 12 Menu Drawer

--- src/partials/Header.js
+++ src/partials/Header.js
@@ -1,28 +1,74 @@
 import {
   AppBar,
   Button,
+  Drawer,
   IconButton,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
   Toolbar,
   Typography,
 } from "@material-ui/core";
+import { useHistory } from "react-router";
+import { useState } from "react";
 
 import MenuIcon from "@material-ui/icons/Menu";
+import HomeIcon from "@material-ui/icons/Home";
+import PersonAddAltIcon from "@material-ui/icons/PersonAdd";
+
 import useStyles from "./Header.style";
 
 const Header = () => {
   const classes = useStyles();
+  const history = useHistory();
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleToggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const handleMenuClick = (route) => {
+    history.push(route);
+    setMenuOpen(false);
+  };
   return (
-    <AppBar position='static' >
-      <Toolbar>
-        <IconButton size='large' edge='start' color='inherit' aria-label='menu'>
-          <MenuIcon />
-        </IconButton>
-        <Typography variant='h6' className={classes.title}>
-          My App
-        </Typography>
-        <Button color='inherit'>Login</Button>
-      </Toolbar>
-    </AppBar>
+    <>
+      <AppBar position='static'>
+        <Toolbar>
+          <IconButton
+            size='large'
+            edge='start'
+            color='inherit'
+            aria-label='menu'
+            onClick={() => handleToggleMenu()}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant='h6' className={classes.title}>
+            My App
+          </Typography>
+          <Button color='inherit'>Login</Button>
+        </Toolbar>
+      </AppBar>
+      <Drawer open={menuOpen} onClose={() => handleToggleMenu()}>
+        <List>
+          <ListItem button onClick={() => handleMenuClick("/")}>
+            <ListItemIcon>
+              <HomeIcon />
+            </ListItemIcon>
+            <ListItemText>Home</ListItemText>
+          </ListItem>
+          <ListItem button onClick={() => handleMenuClick("/customers")}>
+            <ListItemIcon>
+              <PersonAddAltIcon />
+            </ListItemIcon>
+            <ListItemText>Cadastro de Clientes</ListItemText>
+          </ListItem>
+        </List>
+      </Drawer>
+    </>
   );
 };
 export default Header;
